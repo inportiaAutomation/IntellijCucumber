e13d16fa8c3989ed452f93362179cf69c5ff737e
Sohail Choudhary <bash_edu@outlook.com>
2016-11-30 17:41:48 +0530

    Creating properties

--- src/test/java/com/inportia/TestNGDemo/utils/PropertiesManager.java
+++ src/test/java/com/inportia/TestNGDemo/utils/PropertiesManager.java
@@ -2,23 +2,36 @@ package com.inportia.TestNGDemo.utils;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Properties;
 
 public class PropertiesManager 
 {
 	
-	static String properties_location = System.getProperty("user.dir");
+	static String properties_location = System.getProperty("user.dir") + "\\utils\\data\\";
+	private static Properties prop;
+	static OutputStream outFile;
 	
-	public static void create_properties_file(String fileName)
-    {
+	
+	public static void set_property(String key,String value,String fileName)
+	{
 		try
 		{
-		  OutputStream outFile = new FileOutputStream(fileName);
+		    outFile = new FileOutputStream(fileName);
+			prop = new Properties();
+			prop.setProperty(key, value);
+			prop.store(outFile, "Storing the key "+key+" value : "+value);
 		}
 		catch(FileNotFoundException ex)
 		{
 			System.out.println("Error creating a file."+ex.getMessage());
 		}
-    }
+		catch(IOException ex)
+		{
+			System.out.println("Error storing in properties files."+ex.getMessage());
+		}
+		
+	}
 	
 }
